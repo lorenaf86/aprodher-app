22b90f3a4c6026f706ea41f324f04a6071ae8c61
lorenaf86 <lorenaf86@gmail.com>
2017-08-04 11:24:41 -0400

    error de conflicto

--- src/main/java/py/com/app/model/Concurso.java
+++ src/main/java/py/com/app/model/Concurso.java
@@ -7,23 +7,20 @@ package py.com.app.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
+
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
@@ -44,9 +41,6 @@ import javax.xml.bind.annotation.XmlTransient;
     , @NamedQuery(name = "Concurso.findByNombre", query = "SELECT c FROM Concurso c WHERE c.nombre = :nombre")
     , @NamedQuery(name = "Concurso.findByDireccion", query = "SELECT c FROM Concurso c WHERE c.direccion = :direccion")})
 public class Concurso implements Serializable {
-
-    @OneToMany(mappedBy = "concurso", fetch = FetchType.LAZY)
-    private List<ConcursoAcademia> concursoAcademiaList;
 
     private static final long serialVersionUID = 1L;
 
@@ -200,13 +194,4 @@ public class Concurso implements Serializable {
         return "com.aprodher.aprodherweb.Concurso[ id=" + id + " ]";
     }
 
-    @XmlTransient
-    public List<ConcursoAcademia> getConcursoAcademiaList() {
-        return concursoAcademiaList;
-    }
-
-    public void setConcursoAcademiaList(List<ConcursoAcademia> concursoAcademiaList) {
-        this.concursoAcademiaList = concursoAcademiaList;
-    }
-    
 }
